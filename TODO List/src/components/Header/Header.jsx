2dd32e3185448f600a291329a10f548444ddc907
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-08-26 07:18:14 +0530

    todo list -project1

---
+++ TODO List/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>TODO LIST</div>
+  )
+}
+
+export default Header
---
+++ TODO List/src/help.jsx
@@ -0,0 +1,49 @@
+
+    // <Button
+    // m={'0 1.5rem'}
+    //   variant={"solid"}
+    //   fontSize={"1.25rem"}
+    //   p={"5"}
+    //   backgroundColor={'#D6BCFA'}
+    //   color={"black"}
+    //   _hover={{
+    //     backgroundColor: "#805AD5",
+    //     color: "White",
+    //     borderBottom: '4px solid #44337A'
+       
+    //   }}
+    // >
+    //   <Link to={"/"}> Home </Link>
+    // </Button>
+
+    // <Button as="a" href="#about" variant={"solid"}
+    // m={'0 1.5rem'}
+    //       fontSize={"1.25rem"}
+    //       p={"5"}
+    //       backgroundColor={'#D6BCFA'}
+    //       color={"black"}
+    //       _hover={{
+    //         backgroundColor: "#805AD5",
+    //         color: "White",
+    //         borderBottom: '4px solid #44337A'
+    //       }}>
+    //       About
+    // </Button>
+    
+    // <Button  variant={"solid"}
+    // m={'0 1.5rem'}
+    //   fontSize={"1.25rem"}
+    //   p={"5"}
+    //   backgroundColor={'#D6BCFA'}
+    //   color={"black"}
+    //   borderRadius={'12px 0px 12px 12px'}
+    //   _hover={{
+    //     backgroundColor: "#805AD5",
+    //     color: "White",
+    //     borderBottom: '4px solid #44337A',
+    //     transform: 'scale(1.2)'
+       
+    //   }}>
+    //   <Link to={"/login"}>Login </Link>
+    // </Button>
+
